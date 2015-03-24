31fbaf9fba694258566b53ee5cb8c074e5501af8
Ryan La Salle <ryanlasalle@gmail.com>
2015-03-24 02:35:48 -0400

    Add new configs from CM and EOS

---
+++ configs/common_full.mk
@@ -0,0 +1,14 @@
+# Inherit common lpop config
+$(call inherit-product, vendor/lpop/configs/common.mk)
+
+# Optional lpop packages
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam
+
