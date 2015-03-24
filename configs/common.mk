e09d0eb5bf1c03a8514cc5428920fa03393279f6
Ryan La Salle <ryanlasalle@gmail.com>
2015-03-24 00:43:29 -0400

    Add some prop overrides

--- configs/common.mk
+++ configs/common.mk
@@ -6,6 +6,7 @@ LOCAL_PATH:= vendor/lpop/prebuilts/common/system
 
 # Common build prop overrides 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
@@ -13,14 +14,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
     keyguard.no_require_sim=true \
     ro.facelock.black_timeout=400 \
     ro.facelock.det_timeout=1500 \
     ro.facelock.rec_timeout=2500 \
     ro.facelock.lively_timeout=2500 \
     ro.facelock.est_max_time=600 \
-    ro.facelock.use_intro_anim=false
-
+    ro.facelock.use_intro_anim=false \
+    ro.ril.enable.amr.wideband=1 \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    keyguard.no_require_sim=true \
+    drm.service.enabled=true
 # Common overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/lpop/overlays/common
 
